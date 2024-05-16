a4443b38b6b262ba035a709623313b3c1d22b247
Jeffrey <jeffstanley2002@gmail.com>
2024-05-16 21:09:53 +0800

    styling for hometab, added redirects for services

---
+++ src/app/tabs/chatFriends.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const chatFriends = () => {
+  return (
+    <View>
+      <Text>chatFriends</Text>
+    </View>
+  )
+}
+
+export default chatFriends
+
+const styles = StyleSheet.create({})
--- src/app/tabs/home.tsx
+++ src/app/tabs/home.tsx
@@ -1,38 +1,69 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, Alert, Image } from 'react-native';
-import unibudslogo from '@/assets/images/unibuds.png';
+import { View, Text, TextInput, Button, StyleSheet, Alert, Image, TouchableOpacity } from 'react-native';
 import { supabase } from "@/src/supabase/supabase.js";
-
+import Colors from '../../../constants/Colors';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useRouter } from "expo-router";
+import { useNavigation } from '@react-navigation/native';
+import { AntDesign } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 const HomePage = () => {
-
+  const navigation = useNavigation();
   return (
-    <View style={styles.container}>
-      Welcome Home
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>SELECT A SERVICE</Text>
+
+
+      <TouchableOpacity 
+        onPress={() =>navigation.navigate('meetStudents')} 
+        style = {styles.input}> 
+          <FontAwesome5 name="user-friends" size={24} color="black" />
+          <Text style = {{marginLeft: 20, marginRight:180}}>Meet New Students</Text>
+          <AntDesign name="right" size={24} color="black" />
+      </TouchableOpacity>
+
+      <br></br>
+
+      <TouchableOpacity 
+        onPress={() =>navigation.navigate('chatFriends')} 
+        style = {styles.input}> 
+          <AntDesign name="wechat" size={24} color="black" />
+          <Text style = {{marginLeft: 20, marginRight:180}}>Chat With Friends</Text>
+          <AntDesign name="right" size={24} color="black" />
+
+      </TouchableOpacity>
+
+
+      <Text style={styles.title}>DISCOVER</Text>
+
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingHorizontal: 20,
+    padding: 50,
+    borderRadius:20,
+    backgroundColor:'lightpink'
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 20,
+    padding: 20,
   },
   input: {
     height: 40,
+    flexDirection: 'row',
     width: '50%',
-    borderColor: 'gray',
+    borderColor: 'black',
     borderWidth: 1,
-    borderRadius: 5,
+    borderRadius: 10,
     marginBottom: 10,
-    paddingHorizontal: 10,
+    paddingVertical: 50,
+    paddingHorizontal:20,
+    alignItems:'center',
   },
   logo: {
     width: 200,
