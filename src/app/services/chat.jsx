268db01328caa8684e3f040c7438124d791058d1
Ricco Lim <limricco2002@gmail.com>
2024-06-20 05:50:46 +0800

    + Chats & Messages WIP UI

--- src/app/services/chat.jsx
+++ src/app/services/chat.jsx
@@ -9,12 +9,11 @@ import { supabase } from "@/src/supabase/supabase.js";
 
 
 const chat = () => {
-
   const navigation = useNavigation();
   const [messages, setMessage] = useState([]);
-  const [input, setInput] = useState("");
+  const { chatid, userid, chatName: initialChatName } = useLocalSearchParams();
+  const [chatName, setChatName] = useState(initialChatName);
   const [userData, setUserData] = useState(null);
-  const { chatid, userid } = useLocalSearchParams();
 
   useEffect(() => {
     const fetchUserAndMessages = async () => {
@@ -28,14 +27,13 @@ const chat = () => {
       setUserData(user);
 
       // Fetch chats
-      const { data: messages, error: err } = await supabase
+      const { data: DBmessages, error: err } = await supabase
         .from('messages')
         .select('*')
         .eq('chat_id', chatid)
 
       if (err) { console.error(err); return; }
-      let renderedMsgs = [];
-      for (const message of messages) {
+      for (const message of DBmessages) {
         const { id, chat_id, content, created_at, sender_id } = message;
         if (!content) continue;
         let chatObj = {
@@ -46,24 +44,65 @@ const chat = () => {
           // image?: string
           // video?: string
           // audio?: string
-          // system?: boolean
-          // sent?: boolean
-          // received?: boolean
-          // pending?: boolean
+          // system: true,
+          // sent: true,
+          // received: true,
+          // pending: true,
           // quickReplies?: QuickReplies
         }
-        renderedMsgs.push(chatObj);
+        setMessage(messages => GiftedChat.append(messages, chatObj));
       }
-      setMessage(renderedMsgs);
+
+      const subscriptions = supabase
+        .channel('newMessages')
+        .on(
+          'postgres_changes',
+          {
+            event: 'INSERT',
+            schema: 'public',
+            table: 'messages',
+            filter: `chat_id=eq.${chatid}`,
+          },
+          (payload) => {
+            const newMessage = payload.new;
+            const chatObj = {
+              _id: newMessage.id,
+              text: newMessage.content,
+              createdAt: newMessage.created_at,
+              user: { _id: newMessage.sender_id },
+            };
+            setMessage(previousMessages => GiftedChat.append(previousMessages, [chatObj]));
+          }
+        )
+        .on(
+          'postgres_changes',
+          {
+            event: 'UPDATE',
+            schema: 'public',
+            table: 'chats',
+            filter: `id=eq.${chatid}`,
+          },
+          (payload) => {
+            const updatedChat = payload.new;
+            setChatName(updatedChat.chat_name);
+            console.log("newchatname", chatName);
+          }
+        )
+        .subscribe();
+      return () => { supabase.removeChannel(subscriptions); };
     };
     fetchUserAndMessages();
   }, []);
 
   async function onSend(messages) {
     for (const message of messages) {
-      console.log("Sending message=", message);
-      const { _id, text, user: { _id: sender_id } } = message;
-      const { data, error } = await supabase.rpc('sendMessage');
+      const { text, user: { _id: sender_id } } = message;
+      let { error } = await supabase.rpc('sendmessage', {
+        p_chat_id: chatid,
+        p_content: text,
+        p_sender_id: sender_id,
+      })
+      if (error) console.error(error)
     }
   }
 
@@ -75,10 +114,10 @@ const chat = () => {
         </TouchableOpacity>
         <FontAwesome6 name="circle-user" size={30} color="white" style={{ marginLeft: 10 }} />
         <View style={{ flexDirection: 'column', marginLeft: 5 }}>
-          <Text style={styles.headerText}>{chatid} </Text>
+          <Text style={styles.headerText}>{chatName} </Text>
         </View>
       </View>
-      <GiftedChat
+      <GiftedChat 
         messages={messages}
         onSend={messages => onSend(messages)}
         user={{
@@ -94,7 +133,7 @@ export default chat
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // backgroundColor: '#161622',
+    backgroundColor: '#161622',
   },
   headerText: {
     fontSize: 30,
--- src/app/services/chatFriends.jsx
+++ src/app/services/chatFriends.jsx
@@ -6,203 +6,6 @@ import { Ionicons, FontAwesome6, Entypo } from '@expo/vector-icons';
 import { useRouter } from 'expo-router';
 import { supabase } from '@/src/supabase/supabase';
 
-const userChatData = [
-  // Chat data for Alex, Emma, John, and Sophia
-  {
-    id: 1,
-    name: "Alex",
-    age: 21,
-    isOnline: true,
-    lastMessage: "Sure, let's meet there.",
-    date: "2024-05-31",
-    chat: [
-      {
-        sender: "me",
-        message: "Hey Alex, how's it going?",
-        timestamp: "9:00 AM",
-      },
-      {
-        sender: "Alex",
-        message: "Hey, I'm doing well. Just finished studying for EC101.",
-        timestamp: "9:05 AM",
-      },
-      {
-        sender: "me",
-        message: "Nice! How was the study session?",
-        timestamp: "9:10 AM",
-      },
-      {
-        sender: "Alex",
-        message: "It was productive. Got through most of the material.",
-        timestamp: "9:15 AM",
-      },
-      {
-        sender: "me",
-        message: "That's great! Planning anything for the weekend?",
-        timestamp: "9:20 AM",
-      },
-      {
-        sender: "Alex",
-        message: "Not yet. Maybe just relaxing at home. You?",
-        timestamp: "9:25 AM",
-      },
-      {
-        sender: "me",
-        message: "I might go hiking. It's been a while since I've been outdoors.",
-        timestamp: "9:30 AM",
-      },
-      {
-        sender: "Alex",
-        message: "Sounds like a plan. Let me know if you need company!",
-        timestamp: "9:35 AM",
-      },
-    ],
-  },
-  {
-    id: 2,
-    name: "Emma",
-    age: 22,
-    isOnline: false,
-    lastMessage: "No worries, take your time.",
-    date: "2024-05-30",
-    chat: [
-      {
-        sender: "me",
-        message: "Hi Emma, how's your day going?",
-        timestamp: "11:00 AM",
-      },
-      {
-        sender: "Emma",
-        message: "Hey! It's going well, thanks for asking.",
-        timestamp: "11:05 AM",
-      },
-      {
-        sender: "me",
-        message: "That's good to hear. Anything exciting happening?",
-        timestamp: "11:10 AM",
-      },
-      {
-        sender: "Emma",
-        message: "Not really, just catching up on some readings.",
-        timestamp: "11:15 AM",
-      },
-      {
-        sender: "me",
-        message: "Ah, the joys of student life! 😅",
-        timestamp: "11:20 AM",
-      },
-      {
-        sender: "Emma",
-        message: "Indeed! How about you? Any plans?",
-        timestamp: "11:25 AM",
-      },
-      {
-        sender: "me",
-        message: "I might grab lunch with a friend later.",
-        timestamp: "11:30 AM",
-      },
-      {
-        sender: "Emma",
-        message: "Sounds nice. Enjoy your meal!",
-        timestamp: "11:35 AM",
-      },
-    ],
-  },
-  {
-    id: 3,
-    name: "John",
-    age: 20,
-    isOnline: true,
-    lastMessage: "Sure, let's meet at the library.",
-    date: "2024-05-31",
-    chat: [
-      {
-        sender: "me",
-        message: "Hey John, how's it going?",
-        timestamp: "8:00 AM",
-      },
-      {
-        sender: "John",
-        message: "Hey, I'm doing well. Just finished some readings for EC101.",
-        timestamp: "8:05 AM",
-      },
-      {
-        sender: "me",
-        message: "Nice! How are you finding the course?",
-        timestamp: "8:10 AM",
-      },
-      {
-        sender: "John",
-        message: "It's challenging but interesting. How about you?",
-        timestamp: "8:15 AM",
-      },
-      {
-        sender: "me",
-        message: "I'm enjoying it too. Thinking of forming a study group.",
-        timestamp: "8:20 AM",
-      },
-      {
-        sender: "John",
-        message: "Count me in! I could use some study buddies.",
-        timestamp: "8:25 AM",
-      },
-      {
-        sender: "me",
-        message: "Great! Let's meet up tomorrow to discuss.",
-        timestamp: "8:30 AM",
-      },
-      {
-        sender: "John",
-        message: "Sure, let's meet at the library.",
-        timestamp: "8:35 AM",
-      },
-    ],
-  },
-  {
-    id: 4,
-    name: "Sophia",
-    age: 23,
-    isOnline: true,
-    lastMessage: "Definitely! Looking forward to it.",
-    date: "2024-05-30",
-    chat: [
-      {
-        sender: "me",
-        message: "Hey Sophia, how's your week been?",
-        timestamp: "10:00 AM",
-      },
-      {
-        sender: "Sophia",
-        message: "It's been busy but good. How about you?",
-        timestamp: "10:05 AM",
-      },
-      {
-        sender: "me",
-        message: "Mine's been quite hectic too, but managing.",
-        timestamp: "10:10 AM",
-      },
-      {
-        sender: "Sophia",
-        message: "That's good to hear. Any exciting plans for the weekend?",
-        timestamp: "10:15 AM",
-      },
-      {
-        sender: "me",
-        message:
-          "Not yet, but I'm thinking of checking out that new restaurant downtown. You?",
-        timestamp: "10:20 AM",
-      },
-      {
-        sender: "Sophia",
-        message:
-          "I'm planning to go hiking with some friends. You should join us sometime!",
-        timestamp: "10:25 AM",
-      },
-    ],
-  },
-];
-
-
 const chatFriends = () => {
   const navigation = useNavigation();
   const router = useRouter();
@@ -271,23 +74,30 @@ const chatFriends = () => {
             keyExtractor={(item) => item.id}
             renderItem={({ item }) => (
               <TouchableOpacity style={styles.chatItem}
-                onPress={() => router.push({
-                  pathname: 'services/chat', params: { chatid: item.id}
-                })}>
-                <Text>Chat ID: {item.id}</Text>
-                <Text>Chat name: {item.chat_name}</Text>
-                <Text>Participants: {item.participant_ids.join(', ')}</Text>
-                <Text>Created At: {new Date(item.created_at).toLocaleString()}</Text>
-                <Text>Last Chatted at: {item.latest_message_timestamp}</Text>
+                onPress={() => router.push({ pathname: 'services/chat', params: { chatid: item.id, chatName: item.chat_name } })}>
+                <Text style={styles.name}>
+                  <Text>Chat ID: {item.id}</Text>
+                  <Text>Chat name: {item.chat_name}</Text>
+                  <Text>Participants: {item.participant_ids.join(', ')}</Text>
+                  <Text>Created At: {new Date(item.created_at).toLocaleString()}</Text>
+                  <Text>Last Chatted at: {item.latest_message_timestamp}</Text>
+                </Text>
+
+                {/* <Text style={styles.name}>{item.chat_name}{item.latest_message_timestamp}
+                    {item.isOnline && (
+                  <View style={{
+                    width: 5, backgroundColor: 'yellow', height: 5, borderRadius: 3,
+                    marginBottom: 4
+                  }}></View>)} 
+                  </Text> */}
               </TouchableOpacity>
             )} />
         )}
       </View>
 
-      {/* chatview */}
       {/* <View style={{ paddingHorizontal: 20 }}>
         <FlatList
-          data={userChatData}
+          data={chats}
           keyExtractor={(val) => val.id.toString()}
           renderItem={({ item }) => (
             <TouchableOpacity style={{
@@ -326,7 +136,7 @@ export default chatFriends
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // backgroundColor: '#161622',
+    backgroundColor: '#161622',
   },
   headerText: {
     fontSize: 30,
