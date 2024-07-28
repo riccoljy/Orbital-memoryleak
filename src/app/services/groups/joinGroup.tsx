c69db173d85ed961b4b59653ecc3ea1506c05164
riccoljy <limricco2002@gmail.com>
2024-07-28 16:29:07 -0500

    Debug Chat

--- src/app/services/groups/joinGroup.tsx
+++ src/app/services/groups/joinGroup.tsx
@@ -1,23 +1,24 @@
-import React, { useEffect,useState } from 'react';
-import { FlatList,View, Text, StyleSheet, Alert, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { FlatList, View, Text, StyleSheet, Alert, TouchableOpacity } from 'react-native';
 import { supabase } from "@/supabase/supabase";
 import { SafeAreaView } from 'react-native-safe-area-context';
-import {useLocalSearchParams,useNavigation } from "expo-router";
+import { useLocalSearchParams, useNavigation } from "expo-router";
 import { Ionicons } from '@expo/vector-icons';
-import  Collapsible from 'react-native-collapsible';
+import Collapsible from 'react-native-collapsible';
 
 interface Name {
     id: any,
-    name:any
+    name: any
 }
 
 
 const joinGroup = () => {
     const { descrip, name, id, user_name } = useLocalSearchParams();
     const navigation = useNavigation();
-    const [nameData,setNameData] = useState<Name[]|null>(null);
-    const [grpId,setGrpId] = useState(null);
-    const [refresh,setRefresh] = useState(false);
+    const [nameData, setNameData] = useState<Name[] | null>(null);
+    const [userData, setUserData] = useState(null);
+    const [grpId, setGrpId] = useState(null);
+    const [refresh, setRefresh] = useState(false);
 
     const join = async () => {
 
@@ -26,20 +27,47 @@ const joinGroup = () => {
                 .from('join_group')
                 .select('name')
                 .eq('group_id', grpId);
+
             console.log(curData);
+
+            let { data: chats, error } = await supabase
+                .from('chats')
+                .select('*')
+                .eq('group_id', grpId);
+
+            console.log("Chaqweqwets, grpId-", chats[0].id, grpId);
+
             if (!curData?.map(val => val.name).includes(user_name)) {
-                console.log(user_name)
-                const { data, error } = await supabase
-                    .rpc('join_group', { grp_id: grpId, creator: id, user_name: user_name })
+                console.log("Here1")
+                await supabase.rpc('join_group', { grp_id: grpId, creator: id, user_name: user_name })
                 setRefresh(!refresh)
+                console.log("Here2");
 
-                
+                let { data: chats, error } = await supabase
+                    .from('chats')
+                    .select('*')
+                    .eq('group_id', grpId);
 
-                // await supabase
-                //     .rpc('appendparticipants', {
-                //         chat_id,
-                //         new_participant_ids
-                //     })
+                console.log("Chats, grpId-", chats, grpId);
+
+                if (chats && !chats.length && userData) {
+                    await supabase.rpc('createchat', {
+                        chat_name: name,
+                        group_id: grpId,
+                        participant_ids: [userData.id]
+                    })
+                    console.log("Created Grp Chat")
+                }
+
+                else if (chats && chats.length && userData) {
+                    await supabase
+                        .rpc('appendparticipants', {
+                            chat_id: chats[0].id,
+                            new_participant_ids: [userData.id]
+                        })
+                    console.log("Added to Grp Chat")
+                }
+
 
                 Alert.alert('Joined group successfully.')
             }
@@ -54,6 +82,13 @@ const joinGroup = () => {
 
     useEffect(() => {
         const checkSession = async () => {
+
+            const { data: { user }, error: userError } = await supabase.auth.getUser();
+            if (userError || !user) {
+                console.error(userError);
+                return;
+            }
+            setUserData(user);
 
             const { data: grp_id, error: n } = await supabase
                 .from('create_group')
